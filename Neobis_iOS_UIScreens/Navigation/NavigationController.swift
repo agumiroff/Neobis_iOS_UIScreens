a30e85b33cb04504b81cb2adb0dc5ff5388a0546
Askhat Gumirov <agumirovz@gmail.com>
2023-04-05 18:30:45 +0600

    initial commit

---
+++ Neobis_iOS_UIScreens/Navigation/NavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationController.swift
+//  Neobis_iOS_UIScreens
+//
+//  Created by G G on 05.04.2023.
+//
+
+import Foundation
+import UIKit
+
+class Navigation: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Neobis_iOS_UIScreens/Screens/WeatherMainScreenVC.swift
+++ Neobis_iOS_UIScreens/Screens/WeatherMainScreenVC.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherMainScreenVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
